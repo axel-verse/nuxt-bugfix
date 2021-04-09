25b2d1e1c99f49fd94ff58893289ee0290e99ae7
Алескандр 'axel_verse' Мудров <alexander@axel-verse.ml>
2021-04-09 17:12:02 +0300

    Develop branch fixed

---
+++ nuxt.config.js
@@ -0,0 +1,129 @@
+export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      // { hid: 'description', name: 'description', content: '' },
+      { name: 'msapplication-TileColor', content: '#00aba9' },
+      {
+        name: 'msapplication-TileImage',
+        content: '/img/favicons/mstile-144x144.png',
+      },
+      {
+        name: 'msapplication-config',
+        content: '/img/favicons/browserconfig.xml',
+      },
+      { name: 'theme-color', content: '#ffffff' },
+    ],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/img/favicons/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/img/favicons/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '194x194',
+        href: '/img/favicons/favicon-194x194.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        href: '/img/favicons/android-chrome-192x192.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/img/favicons/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/img/favicons/site.webmanifest' },
+      {
+        rel: 'mask-icon',
+        href: '/img/favicons/safari-pinned-tab.svg',
+        color: '#00aba9',
+      },
+      { rel: 'shortcut icon', href: '/img/favicons/favicon.ico' },
+      {
+        rel: 'preload',
+        href: '/img/logo.svg',
+        as: 'image',
+        type: 'image/svg+xml',
+      },
+    ],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: ['@/assets/global.scss'],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/fontawesome',
+    '@nuxtjs/moment',
+    '@nuxt/image',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Content module configuration (https://go.nuxtjs.dev/config-content)
+  content: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+
+  fontawesome: {
+    component: 'Fa',
+    suffix: true,
+    icons: {
+      solid: ['faCalendarAlt', 'faEdit', 'faBars'],
+    },
+  },
+
+  moment: {
+    defaultLocale: 'ru',
+    defaultTimezone: 'Europe/Moscow',
+    locales: ['ru'],
+  },
+
+  render: {
+    asyncScripts: true,
+  },
+
+  generate: {
+    fallback: '404.html',
+  },
+}
---
+++ pages/posts/_slug/index.vue
@@ -0,0 +1,107 @@
+<template>
+  <div v-if="post" class="post">
+    <div class="post-img">
+      <nuxt-picture
+        class="h-full w-full"
+        width="100%"
+        height="24rem"
+        :src="`/img/blog/${post.img}`"
+        :placeholder="true"
+        fit="cover"
+        sizes="586,768:768,1024:1024,1280:1280,1536:1536,1900:1900"
+      />
+      <div class="post-img-heading">
+        <h2 class="">{{ post.title }}</h2>
+      </div>
+    </div>
+    <div class="container">
+      <div class="post-dates">
+        <div class="post-dates-created">
+          <fa-icon icon="calendar-alt" class="mr-1" />
+          {{ $moment(post.createdAt).format('dd, DD MMM YYYY, HH:mm:ss') }}
+        </div>
+        <div class="post-dates-updated">
+          <fa-icon icon="edit" class="mr-1" />
+          {{ $moment(post.updatedAt).format('dd, DD MMM YYYY, HH:mm:ss') }}
+        </div>
+      </div>
+      <nuxt-content :document="post" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.post {
+  @apply sm:mb-10;
+
+  &-img {
+    @apply h-96 w-full bg-white shadow relative overflow-hidden;
+    @apply sm:mb-10;
+
+    &-heading {
+      @apply p-4 bg-white bg-opacity-75 w-full text-center shadow absolute top-1/2 transform -translate-y-1/2;
+      backdrop-filter: blur(8px);
+
+      h2 {
+        @apply text-2xl font-light tracking-tight;
+        @apply sm:text-4xl;
+      }
+    }
+  }
+
+  .container {
+    @apply mx-auto p-4 bg-white;
+    @apply sm:rounded-xl;
+
+    .post-dates {
+      @apply text-gray-500 font-medium text-xs tracking-wide flex items-center justify-between flex-wrap mb-4;
+      @apply sm:justify-start;
+
+      &-updated,
+      &-created {
+        @apply flex-none;
+      }
+
+      &-created {
+        @apply sm:mr-16;
+      }
+    }
+
+    p:not(:last-child) {
+      @apply mb-4;
+    }
+  }
+}
+</style>
+
+<script>
+export default {
+  async asyncData({ $content, params, redirect }) {
+    const slug = params.slug
+    const pageContent = await $content('blog', slug)
+      .fetch()
+      .catch(() => {
+        redirect(302, '/')
+      })
+    return { post: pageContent }
+  },
+  computed: {
+    bpList() {
+      return this.$store.state.breakpoints.bpList
+    },
+  },
+  head() {
+    return {
+      title: this.post && this.post.title ? this.post.title : null,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            this.post && this.post.description ? this.post.description : null,
+        },
+      ],
+    }
+  },
+}
+</script>
