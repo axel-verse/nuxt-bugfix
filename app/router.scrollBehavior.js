fc461ac489b0e51b9f1e9d6b103b6641cdf45ae1
Алескандр 'axel_verse' Мудров <alexander@axel-verse.ml>
2021-07-19 20:43:44 +0300

    WIP

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,27 +1,3 @@
 export default function (to, from, savedPosition) {
-  // return { x: 0, y: 0 }
-
-  const nuxt = window['<%= globals.nuxt %>']
-
-  if (to.path === from.path) {
-    return
-  }
-  if (to.hash) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          selector: to.hash,
-        })
-      }, 500)
-    })
-  }
-
-  const position = savedPosition || { x: 0, y: 0 }
-  return new Promise((resolve, reject) => {
-    nuxt.$root.$once('triggerScroll', () => {
-      nuxt.$nextTick(() => {
-        resolve(position)
-      })
-    })
-  })
+  return { x: 0, y: 0 }
 }
